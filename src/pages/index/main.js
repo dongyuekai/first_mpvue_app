0e452abf4aa4904ac108a0235ea93e9d0565d855
dyk <dongyuekai@autohome.com.cn>
2018-08-14 15:57:24 +0800

    初始化一个mpvue开发小程序的项目

---
+++ src/pages/index/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import App from './index'
+
+const app = new Vue(App)
+app.$mount()
+
+export default {
+    config: {
+        // 页面级可配置的属性相当于只是‘src/main.js’中配置的‘window’部分
+        "navigationBarTitleText": "文章列表页面"
+    }
+}
