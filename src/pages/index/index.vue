a37de6126600624d6a7eb7a324c83fb9e2dfdfae
dyk <dongyuekai@autohome.com.cn>
2018-08-14 16:44:25 +0800

    实现vue组件的复用、传值、占坑功能

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,10 +1,19 @@
 <template>
     <div class="container" @click="clickHandle">
         <div class="message">{{msg}}</div>
+        <click-counter :init-num="10" @clicknum="handleClickNum"/>
+        <click-counter :init-num="20" @clicknum="handleClickNum"/>
+        <click-counter :init-num="30" @clicknum="handleClickNum">
+            <!--填坑用-->
+            <input type="checkbox"/>
+        </click-counter>
     </div>
 </template>
 <script>
+// 导入 click-counter组件
+import clickCounter  from '@/components/click-counter';
 export default {
+  components: { clickCounter },
   data() {
     return {
       msg: "Hello"
@@ -13,8 +22,12 @@ export default {
   methods: {
     clickHandle() {
       this.msg = "Clicked!!!!!"
+    },
+    handleClickNum(data){
+        console.log(">>>>>>>>>>",data.num);
     }
-  }
+  },
+
 }
 </script>
 <style scoped>
